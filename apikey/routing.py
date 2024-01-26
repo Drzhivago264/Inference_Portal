cb0706e4fb3adc218e34ec63853be15083c6526e
h <hoangnv.yec15@gmail.com>
2024-01-27 10:59:37 +1100

    update dependencies

--- apikey/routing.py
+++ apikey/routing.py
@@ -1,7 +1,7 @@
-from django.urls import re_path
+from django.urls import re_path, path
 
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/(?P<name>\w+)/(?P<key>\w+)/$", consumers.ChatConsumer.as_asgi()),
+    path("ws/chat/<str:name>/<str:key>/",consumers.ChatConsumer.as_asgi()),
 ]
