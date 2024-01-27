7ad9502407803290ce4f073d93712921f063a0f7
h <hoangnv.yec15@gmail.com>
2024-01-27 13:18:13 +1100

    production setting

--- inferenceportal/asgi.py
+++ inferenceportal/asgi.py
@@ -7,13 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inferenceportal.settings')
+django.setup()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
 from django.core.asgi import get_asgi_application
 import apikey.routing
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
--- inferenceportal/settings.py
+++ inferenceportal/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^$&ykn@si0(+!)h_rh)pxk0bd05_3oe3m+-kuw+-vqk9u#q$m2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
+
 
 
 # Application definition
