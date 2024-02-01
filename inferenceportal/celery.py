579f2fda31c7baad2e3f4c17b2d511b8141ab50c
h <hoangnv.yec15@gmail.com>
2024-02-02 00:59:41 +1100

    periodically update EC2 status

--- inferenceportal/celery.py
+++ inferenceportal/celery.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 from django.conf import settings
-
+import apikey
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inferenceportal.settings')
 
@@ -13,3 +13,11 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load tasks from all registered Django app configs.
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+app.conf.beat_schedule = {
+    'periodically_monitor_EC2_instance': {
+        'task': 'apikey.celery_tasks.periodically_monitor_EC2_instance',      
+        'schedule': 120,
+
+    },
+}
