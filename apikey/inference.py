6d53dc3523ecfc2eddc9269219c9f3adc4aa8b5e
h <hoangnv.yec15@gmail.com>
2024-02-27 04:40:14 +1100

    put key in header

--- apikey/inference.py
+++ apikey/inference.py
@@ -26,11 +26,11 @@ ami = "ami-0810c2d824776b340"
 
 
 
-""" print(template.format(promtp))
+print(template.format(promtp))
 for i in range(2):
-    response = requests.post("http://127.0.0.1:8000/api/",   json=context ) 
+    response = requests.post("http://127.0.0.1:8000/api/", headers = {"Authorization": "test"},   json=context ) 
     print(response.json())
- """
+
 p = """Below is an instruction that describes a task. Write a response that appropriately completes the request.
 
 
@@ -305,7 +305,7 @@ Hello
 Hello! How can I assist you today?
 
 """
-response = requests.post("http://3.93.75.76/generate", headers = {"User-Agent": "Test Client"},  json={ "prompt": p,   "temperature": 0, "stream" : True}
+""" response = requests.post("http://3.93.75.76/generate", headers = {"User-Agent": "Test Client"},  json={ "prompt": p,   "temperature": 0, "stream" : True}
                          , stream = True ) 
 previous_output = str()
 for chunk in response.iter_lines(chunk_size= 1024,
@@ -317,3 +317,4 @@ for chunk in response.iter_lines(chunk_size= 1024,
         output = data["text"][0]
         print(output.replace(previous_output, ""))
         previous_output = output
+ """
