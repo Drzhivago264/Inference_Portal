337a5a0b950edb5f8160b98618fffee5c915ad5b
h <hoangnv.yec15@gmail.com>
2024-05-28 23:54:38 +1000

    first step to support multiple language

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
-
+import './i18n';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
