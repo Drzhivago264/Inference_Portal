6e2178e88110984d42a0bd74405026da21dc232f
h <hoangnv.yec15@gmail.com>
2024-07-10 19:07:54 +1000

    move sqlite to postgres

--- inferenceportal/settings.py
+++ inferenceportal/settings.py
@@ -104,8 +104,12 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesSto
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'professorparakeet', 
+        'USER': config("POSTGRES_USER"),
+        'PASSWORD': config("POSTGRES_PASSWORD"),
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 # Password validation
