8028971c395340968cfe2edb2f789f02c49a74ee
h <hoangnv.yec15@gmail.com>
2024-09-20 23:34:52 +1000

    lastest

--- server/webhooks/lark_base_button_webhook.py
+++ server/webhooks/lark_base_button_webhook.py
@@ -12,16 +12,27 @@ import requests
 import openai
 from decouple import config
 from constance import config as constant
+from pydantic import BaseModel
 
 
-def get_root_folder(access_token):
-    root_folder = "https://open.larksuite.com/open-apis/drive/explorer/v2/root_folder/meta"
+
+class ParagraphSchema(BaseModel):
+    type: str
+    content: str
+    paragraph: str | None
+
+class DocumentSchema(BaseModel):
+    document_structure: list[ParagraphSchema]
+    document_description: str
+def get_folder_list(access_token: str, root: bool, folder_token: str | None = None):
+    url = "https://open.larksuite.com/open-apis/drive/explorer/v2/root_folder/meta" if root else f"https://open.larksuite.com/open-apis/drive/explorer/v2/folder/{folder_token}/children"
     headers = {
         'Authorization': f'Bearer {access_token}'
     }
     root_folder_response = requests.request(
-        "GET", root_folder, headers=headers)
-    return root_folder_response
+        "GET", url, params={"types":"folder"} if not root else {}, headers=headers)
+    print(root_folder_response.json())
+    return root_folder_response.json()
 
 
 def create_folder(access_token, root_folder_token):
@@ -51,42 +62,106 @@ def get_doc_sharing_setting(access_token, doc_token):
         "POST", url, headers=headers, data=json.dumps(data))
     print(settings.json())
 
-
-def get_app_access_token():
-    url = "https://open.larksuite.com/open-apis/auth/v3/app_access_token/internal"
-    data = {"app_id": config("LARK_APP_ID"),
-            "app_secret": config("LARK_APP_SECRET")}
-    headers = {"Content-Type": "application/json; charset=utf-8"}
-    response = requests.request(
-        "POST", url, headers=headers, data=json.dumps(data))
-    access_token = response.json()["app_access_token"]
-    cache.set(key="larksuite_app_access_token",
-              value=access_token, timeout=7000)
-
-
-def get_user_access_token(app_access_token):
-    url = "https://open.larksuite.com/open-apis/authen/v1/oidc/access_token"
+def get_group_id(group_name: str, access_token: str):
+    url = "https://open.larksuite.com/open-apis/im/v1/chats"
     headers = {
-        'Authorization': f'Bearer {app_access_token}',
+        'Authorization': f'Bearer {access_token}'
+    }
+    response = requests.request("GET", url, headers=headers)
+    response =response.json()
+    has_group = False
+    for group in response["data"]["items"]: 
+        if group["name"] == group_name:
+            has_group = True
+            return group["chat_id"]
+    if not has_group:
+        return False
+def add_group_permission(access_token: str, group_id: str):
+    url = f"https://open.larksuite.com/open-apis/drive/v1/permissions/Bmlgff7eSlXECKdHgjPlAY4xgMf/members"
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    data = {
+        "member_type": "openchat",
+        "member_id": group_id,
+        "perm": "full_access"
+    }
+    response = requests.request(
+        "POST", url, headers=headers, params={"type": "folder"} ,data=json.dumps(data))
+    print(response.json())
+
+def submit_doc(response: DocumentSchema, access_token: str):
+    url = "https://open.larksuite.com/open-apis/doc/v2/create"
+    test_doc = {"FolderToken": "Bmlgff7eSlXECKdHgjPlAY4xgMf", "Content": ""}
+    doc_content = {
+        "title": {
+            "elements": [
+                {
+                    "type": "textRun",
+                    "textRun": {
+                        "text": "",
+                        "style": {}
+                    }
+                }
+            ]
+        },
+        "body": {
+            "blocks": []
+        }
+    }
+    document_structure = response.document_structure
+    document_description = response.document_description
+    for component in document_structure:
+        if component.type == "h1":
+            doc_title = component.content
+            doc_content["title"]["elements"][0]["textRun"]["text"] = doc_title
+        elif component.type == "h2":
+            paragraph_content = [
+                {
+                    "type": "paragraph",
+                    "paragraph": {
+                        "elements": [
+                            {
+                                "type": "textRun",
+                                "textRun": {
+                                    "text": component.content
+                                }
+                            }
+                        ],
+                        "style": {"headingLevel": 2}
+                    }
+                },
+                {
+                    "type": "paragraph",
+                    "paragraph": {
+                        "elements": [
+                            {
+                                "type": "textRun",
+                                "textRun": {
+                                    "text": component.paragraph,
+                                    "style": {}
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
+            doc_content["body"]["blocks"] += paragraph_content
+    doc_content_to_string = json.dumps(doc_content)
+    test_doc["Content"] = doc_content_to_string
+    payload = json.dumps(test_doc)
+    headers = {
+        'Authorization': f'Bearer {access_token}',
         'Content-Type':  'application/json; charset=utf-8'
     }
-    code = get_oauth_code()
-    data = {
-        "grant_type": "authorization_code",
-        "code": code
-    }
-    response = requests.request(
-        "POST", url, headers=headers, data=json.dumps(data))
-    print(response.json())
-    return response.json()["code"]
-
-
-def get_oauth_code():
-    url = f'https://open.larksuite.com/open-apis/authen/v1/authorize?app_id={config("LARK_APP_ID")}&redirect_uri=https://lamb-diverse-ram.ngrok-free.app/webhooks/lark-base-button/'
-    response = requests.request("GET", url)
-    print(response.content)
-    return
-
+    doc_response = requests.request("POST", url, headers=headers, data=payload)
+    doc_response = doc_response.json()
+    if doc_response['code'] == 99991663: #Invalid token (expired)
+        access_token = get_tenant_access_token()
+        headers['Authorization'] = f'Bearer {access_token}'
+        doc_response = requests.request("POST", url, headers=headers, data=payload)
+        doc_response = doc_response.json()
+    return doc_response, doc_title, document_description
 
 def update_doc_sharing_setting(access_token, doc_token):
     url = f"https://open.larksuite.com/open-apis/drive/v1/permissions/{doc_token}/public"
@@ -107,29 +182,9 @@ def update_doc_sharing_setting(access_token, doc_token):
     print(response.json())
 
 
-def move_doc_to_shared_folder(folder_token, file_token, tenant_access_token):
-    url = f"https://open.larksuite.com/open-apis/drive/v1/files/{file_token}/move"
-    headers = {
-        'Authorization': f'Bearer {tenant_access_token}'
-    }
-    data = {
-        "type": "doc",
-        "folder_token": folder_token
-    }
-    response = requests.request(
-        "POST", url, headers=headers, data=json.dumps(data))
-    print(response.json())
+def create_doc(key_word: str, product_name: str, shared_group: str):
 
-
-def create_doc(key_word: str, product_name: str):
-    # 
     tenant_access_token = cache.get(key="larksuite_tenant_access_token")
-    #app_access_token = cache.get(key="larksuite_app_access_token")
-    #user_access_token = cache.get(key="lark_suite_user_access_token")
-    #if app_access_token is None:
-    #    app_access_token = get_app_access_token()
-    #if user_access_token is None:
-    #    user_access_token = get_user_access_token(app_access_token=app_access_token)
     if tenant_access_token is None:
         tenant_access_token = get_tenant_access_token()
 
@@ -139,16 +194,18 @@ def create_doc(key_word: str, product_name: str):
         max_retries=constant.RETRY,
     )
     try:
-        raw_response = client.chat.completions.create(
+        raw_response = client.beta.chat.completions.parse(
             model="gpt-4o-mini",
             messages=[
                 {"role": "system", "content": 'You are a helpful SEO expert who need to write document base on the keywords provided by user \
-                You need to put each paragraph of the document into a json with this format [{{"h1": "The title of the document", "desc": "Page Meta Description for SEO that contain key words in {} to promote product: {} "}}, {{"h2": "The title of the first paragraph", "paragraph": "The content of the fisrt paragraph" }}, {{"h2": "The title of the second paragraph", "paragraph": "The content of the second paragraph"}}]'.format(key_word, product_name)},
+                You need to put each paragraph of the document into a json with this format {{ {{"document_structure": [{{"type": "h1", "content": "The title of the document"}}, {{ "type": "h2", "content": "The title of the first paragraph", "paragraph": "The content of the fisrt paragraph" }}, {{"type": "h2", "content": "The title of the second paragraph", "paragraph": "The content of the second paragraph"}}]}}, {{ "document_description": "Page Meta Description for SEO that contain key words in {} to promote product: {} "}}  }}'.format(key_word, product_name)},
                 {"role": "user", "content": f"Write SEO document for the following key words: {key_word}"},
             ],
-            stream=False
+            response_format=DocumentSchema
         )
-        response = json.loads(raw_response.choices[0].message.content)
+        print(raw_response.choices[0].message.parsed)
+        response = raw_response.choices[0].message.parsed
+
 
     except openai.APIConnectionError as e:
         response = f"Failed to connect to inference server: {e}"
@@ -159,78 +216,24 @@ def create_doc(key_word: str, product_name: str):
     except openai.APIError as e:
         response = f"Inference server returned an API Error: {e}"
         return "", response, response
-    except ValueError:
+    except ValueError as e:
         response = "Failed to parse the response from openai, try again."
         return "", response, response
 
-    url = "https://open.larksuite.com/open-apis/doc/v2/create"
-    test_doc = {"FolderToken": "", "Content": ""}
-    doc_content = {
-        "title": {
-            "elements": [
-                {
-                    "type": "textRun",
-                    "textRun": {
-                        "text": "",
-                        "style": {}
-                    }
-                }
-            ]
-        },
-        "body": {
-            "blocks": []
-        }
-    }
-    for component in response:
-        if "h1" in component:
-            doc_title = component["h1"]
-            doc_desc = component["desc"]
-            doc_content["title"]["elements"][0]["textRun"]["text"] = doc_title
-        elif "h2" in component:
-            paragraph_content = [
-                {
-                    "type": "paragraph",
-                    "paragraph": {
-                        "elements": [
-                            {
-                                "type": "textRun",
-                                "textRun": {
-                                    "text": component["h2"]
-                                }
-                            }
-                        ],
-                        "style": {"headingLevel": 2}
-                    }
-                },
-                {
-                    "type": "paragraph",
-                    "paragraph": {
-                        "elements": [
-                            {
-                                "type": "textRun",
-                                "textRun": {
-                                    "text": component["paragraph"],
-                                    "style": {}
-                                }
-                            }
-                        ]
-                    }
-                }
-            ]
-            doc_content["body"]["blocks"] += paragraph_content
-    doc_content_to_string = json.dumps(doc_content)
-    test_doc["Content"] = doc_content_to_string
-    payload = json.dumps(test_doc)
-    headers = {
-        'Authorization': f'Bearer {tenant_access_token}',
-        'Content-Type':  'application/json; charset=utf-8'
-    }
-    doc_response = requests.request("POST", url, headers=headers, data=payload)
-    doc_response = doc_response.json()
+    doc_response, doc_title, document_description = submit_doc(access_token=tenant_access_token, response=response)
+    print(doc_response)
     update_doc_sharing_setting(
        access_token=tenant_access_token, doc_token=doc_response["data"]["objToken"])
-
-    return doc_response['data']['url'], doc_title, doc_desc
+    # - The folder token of Doc Bot is "nodlgekDroihB8PlH3czpay50pb"
+    # - If you need to get it again: get_folder_list(access_token=tenant_access_token, root=True)
+    # - The shared folder token of Doc Bot is "Bmlgff7eSlXECKdHgjPlAY4xgMf" and the name of the folder is: "Seo Output"
+    # - If you want to get it again or get another folder: get_folder_list(access_token=tenant_access_token, root=False, folder_token="nodlgekDroihB8PlH3czpay50pb") 
+    group_id = get_group_id(access_token=tenant_access_token, group_name=shared_group)
+    if group_id:
+        add_group_permission(access_token=tenant_access_token, group_id=group_id)
+        return doc_response['data']['url'], doc_title, document_description
+    else:
+        return "Bot was not added to the group", "Bot was not added to the group", "Bot was not added to the group"
 
 
 @api_view(["POST"])
@@ -240,7 +243,8 @@ def lark_base_button_webhook(request: HttpRequest) -> Response:
     post_data = json.loads(request.body.decode())
     key_word = post_data["key_word"]
     product_name = post_data["product_name"]
-    doc_url, title, desc = create_doc(key_word, product_name)
+    shared_group = post_data["shared_group"] if post_data["shared_group"] else "Engineering"
+    doc_url, title, desc = create_doc(key_word, product_name, shared_group)
     seo_desc = desc
     seo_title = title
     return Response({"seo_description_response": seo_desc, "seo_title": seo_title, "doc_url": doc_url}, status=status.HTTP_200_OK)
--- server/webhooks/lark_webhook.py
+++ server/webhooks/lark_webhook.py
@@ -60,7 +60,7 @@ def get_tenant_access_token():
     payload = json.dumps(req)
     response = requests.request("POST", url, headers=headers, data=payload)
     access_token = response.json()["tenant_access_token"]
-    cache.set(key="larksuite_tenant_access_token", value=access_token, timeout=7000)
+    cache.set(key="larksuite_tenant_access_token", value=access_token, timeout=120)
     return access_token
 
 
